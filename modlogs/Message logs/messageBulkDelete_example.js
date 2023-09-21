a80a468c32781e95dd6ddd6ccb913e8b7128d61b
Phytone <74849071+EnderSlain456@users.noreply.github.com>
2023-09-21 23:39:44 +0100

    Message BulkDelete log

---
+++ modlogs/Message logs/messageBulkDelete_example.js
@@ -0,0 +1,29 @@
+const { Events, AuditLogEvent, EmbedBuilder } = require('discord.js')
+
+module.exports = {
+  name: Events.MessageBulkDelete,
+  async execute (messages) {
+    messages.first().guild.fetchAuditLogs({
+      type: AuditLogEvent.MessageBulkDelete
+    })
+      .then(async audit => {
+        const entry = audit.entries.first()
+        if (!entry) return
+
+        const { executor } = entry
+
+        const mChannel = await messages.first().guild.channels.cache.get('YOUR_CHANNEL_ID')
+
+        const MessageBulkDeleteEmbed = new EmbedBuilder()
+          .setColor('Red')
+          .setTitle('Messages Bulk Deleted')
+          .addFields({ name: 'Amount Deleted', value: `${messages.size}`, inline: false })
+          .addFields({ name: 'Message Channel', value: `${messages.first().channel}`, inline: false })
+          .addFields({ name: 'Deleted By', value: `${executor.tag}`, inline: false })
+          .setTimestamp()
+          .setFooter({ text: 'Mod logs ' })
+
+        mChannel.send({ embeds: [MessageBulkDeleteEmbed] })
+      })
+  }
+}
