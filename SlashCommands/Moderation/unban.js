fa094dcc5fa6c09201be33324f6a5eb13a78fa17
Phytone <74849071+EnderSlain456@users.noreply.github.com>
2023-09-21 14:19:16 +0100

    Added Unban Command

---
+++ SlashCommands/Moderation/unban.js
@@ -0,0 +1,37 @@
+// Unban Command made with SlashCommandBuilder
+
+const { SlashCommandBuilder, EmbedBuilder, PermissionsBitField } = require('discord.js')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('unban')
+    .setDescription('unbans a member from the server')
+    .addUserOption(option => option.setName('member').setDescription('The member you want to unban').setRequired(true)),
+  async execute (interaction) {
+    const userID = interaction.options.getUser('member')
+
+    if (!interaction.member.permissions.has(PermissionsBitField.Flags.BanMembers)) return await interaction.reply({ content: 'You Do Not Have Permission to run this Command!' })
+    if (interaction.member.id === userID) return await interaction.reply({ content: 'You Cannot Ban YourSelf', ephemeral: true })
+
+    const UnbanEmbed = new EmbedBuilder()
+      .setColor('Red')
+      .setDescription(`${userID} Has been unbanned`)
+
+    await interaction.guild.bans.fetch()
+      .then(async bans => {
+        // eslint-disable-next-line eqeqeq
+        if (bans.size == 0) return await interaction.reply({ content: 'There is no one banned from this guild', ephemeral: true })
+        // eslint-disable-next-line eqeqeq
+        const bannedID = bans.find(ban => ban.user.id == userID)
+        if (!bannedID) return await interaction.reply({ content: 'The ID Stated is not banned from the server', ephemeral: true })
+
+        await interaction.guild.bans.remove(userID).catch(err => {
+          console.log(err)
+          return interaction.reply({ content: 'I cannot unban this member' })
+        })
+      })
+
+    await interaction.reply({ embeds: [UnbanEmbed] })
+  }
+
+}
